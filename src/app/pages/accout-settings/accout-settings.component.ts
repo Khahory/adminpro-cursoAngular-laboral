b6573c71f34f3ff80111d370b514a9ddfc514f88
angelmaria <angeld05@live.com>
2019-09-10 15:34:05 -0400

    Seleccion de temas agregado

--- src/app/pages/accout-settings/accout-settings.component.ts
+++ src/app/pages/accout-settings/accout-settings.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-accout-settings',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccoutSettingsComponent implements OnInit {
 
-  constructor() { }
+  // tslint:disable-next-line:variable-name
+  constructor(@Inject(DOCUMENT) private _document) { }
 
   ngOnInit() {
+  }
+
+  private cambiarColor(tema: string) {
+    console.log(tema);
+    const url = `assets/css/colors/${tema}.css`;
+    this._document.getElementById('tema').setAttribute('href', url);
   }
 
 }
