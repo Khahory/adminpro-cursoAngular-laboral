33a48c3c30f1da1996e6d38f43a1c6af3d191174
angelmaria <angeld05@live.com>
2019-09-10 09:02:30 -0400

    Reutilizando incrementador progressbar

---
+++ src/app/components/incrementador/incrementador.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-incrementador',
+  templateUrl: './incrementador.component.html',
+  styles: []
+})
+export class IncrementadorComponent implements OnInit {
+  // Variables
+  private progreso: number = 60;
+  private leyenda: string = 'Leyenda';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  private cambiarValor(valor: number) {
+    if (this.progreso >= 100 && valor > 0) {
+      this.progreso = 100;
+      return;
+    }
+
+    if (this.progreso <= 0 && valor < 0) {
+      this.progreso = 0;
+      return;
+    }
+
+    this.progreso = this.progreso + valor;
+  }
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,18 +4,22 @@ import { NgModule } from '@angular/core';
 import {SharedModule} from '../shared/shared.module';
 import {FormsModule} from '@angular/forms';
 
+
+
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {ProgressComponent} from './progress/progress.component';
 import {Graficas1Component} from './graficas1/graficas1.component';
 import {PagesComponent} from './pages.component';
 import {PAGES_ROUTES} from './pages.routes';
+import {IncrementadorComponent} from '../components/incrementador/incrementador.component';
 
 @NgModule({
   declarations: [
     PagesComponent,
     DashboardComponent,
     ProgressComponent,
-    Graficas1Component
+    Graficas1Component,
+    IncrementadorComponent
   ],
   exports: [    // La exportamos porque requerimos estos componente en otrod componente que no estan aqui
     DashboardComponent, // Ej: si solo usaramos 'Graficas1' en 'Dashboard o Progress'
