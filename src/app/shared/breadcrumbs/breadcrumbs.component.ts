2c768bd672f104523c8c3455208f565594aea872
angelmaria <angeld05@live.com>
2019-09-11 15:02:13 -0400

    Add - titulos y metaTag(no funciona) dinamicos

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivationEnd, Router} from '@angular/router';
 import {filter, map} from 'rxjs/operators';
+import {Title, Meta, MetaDefinition} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -10,11 +11,25 @@ import {filter, map} from 'rxjs/operators';
 export class BreadcrumbsComponent implements OnInit {
   //  Variables
   private titulo: string;
-  constructor(private router: Router) {
+
+  constructor( private router: Router,
+               private titlle: Title,
+               private meta: Meta) {
+
     this.getDataRouter()
       .subscribe(data => {
         console.log(data);
         this.titulo = data.titulo;
+        this.titlle.setTitle(this.titulo);
+
+        //  Agregando meta datos
+        const metaTag: MetaDefinition = {
+          content: this.titulo,
+          name: 'description'
+        };
+
+        this.meta.updateTag(metaTag);
+        console.log(metaTag);
       });
   }
 
