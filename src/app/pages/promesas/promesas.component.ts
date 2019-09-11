88681fc6417d85fb8eb168b68060e69f51cd1d06
angelmaria <angeld05@live.com>
2019-09-11 11:47:10 -0400

    Funcion que retorna una promesa

--- src/app/pages/promesas/promesas.component.ts
+++ src/app/pages/promesas/promesas.component.ts
@@ -8,26 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class PromesasComponent implements OnInit {
 
   constructor() {
-  //  Promesa para que se ejecute y termine en 3 segundos
-    let promesa = new Promise((resolve, reject) => {
-    let contador = 0;
-
-      //  Cuando el contador llegue a 3 pues muere, eso hace el intervalo que agregammos mejor
-      let intervalo = setInterval(() => {
-          contador += 1;
-          console.log(contador);
-          if (contador === 3) {
-            reject('Simplemente murio');
-            clearInterval(intervalo);
-          }
-      }, 1000);
-    });
-    promesa.then(
-      () => console.log('Termino la promesa')
+    this.contarATres().then(
+      mensaje => console.log(mensaje, 'Ok')
     ).catch(error => console.error('error de la promesa', error));
   }
 
   ngOnInit() {
+  }
+
+  // @ts-ignore
+  private contarATres(): Promise<boolean> {
+    //  Promesa para que se ejecute y termine en 3 segundos
+    return new Promise((resolve, reject) => { //resolve = salio bien, reject = salio mal
+      let contador = 0;
+
+      //  Cuando el contador llegue a 3 pues muere, eso hace el intervalo que agregammos mejor
+      let intervalo = setInterval(() => {
+        contador += 1;
+        console.log(contador);
+        if (contador === 3) {
+          reject(true);
+          clearInterval(intervalo);
+        }
+      }, 1000);
+    });
   }
 
 }
